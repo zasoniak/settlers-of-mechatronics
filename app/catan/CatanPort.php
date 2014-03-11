b1c6b32f68a54e30662c6b5f03fd9a4b30def72c
mroova <mroova@Mroovuntu>
2014-03-11 11:55:58 +0100

    Ports tweak

--- app/catan/CatanPort.php
+++ app/catan/CatanPort.php
@@ -22,44 +22,61 @@ class CatanPort implements DrawableInterface
     {
       $return =  '<div class="port ';
       $return .= $this->model->type;
-      $return .= '" style="left: ';
       if($this->model->x==-10)
       {
+        $return .= ' dir4';
+        $return .= '" style="left: ';
         $return .= '420px; top: 618px;"';
       }
       elseif($this->model->x==0)
       {
+        $return .= ' dir4';
+        $return .= '" style="left: ';
         $return .= '180px; top: 0px;"';
       }
       elseif($this->model->x==10)
       {
+        $return .= ' dir4';
+        $return .= '" style="left: ';
         $return .= '600px; top: 515px;"';
       }
       elseif($this->model->y==-10)
       {
+        $return .= ' dir0';
+        $return .= '" style="left: ';
         $return .= '600px; top: 103px;"';
       }
       elseif($this->model->y==0)
       {
+        $return .= ' dir3';
+        $return .= '" style="left: ';
         $return .= '180px; top: 618px;"';
       }
       elseif($this->model->y==10)
       {
+        $return .= ' dir0';
+        $return .= '" style="left: ';
         $return .= '420px; top: 0px;"';
       }
       elseif($this->model->z==-10)
       {
+        $return .= ' dir2';
+        $return .= '" style="left: ';
         $return .= '60px; top: 206px;"';
       }
       elseif($this->model->z==0)
       {
+        $return .= ' dir5';
+        $return .= '" style="left: ';
         $return .= '720px; top: 309px;"';
       }
       else
       {
+        $return .= ' dir2';
+        $return .= '" style="left: ';
         $return .= '60px; top: 412px;"';
       }
-      $return .= '>'.$this->model->x.';'.$this->model->y.';'.$this->model->z.'</div>';
+      $return .= '></div>';
       return $return;
     }
 }
